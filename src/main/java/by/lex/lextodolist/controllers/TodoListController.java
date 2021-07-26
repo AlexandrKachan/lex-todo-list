3823fbe172e495ff26ccd894f511327cdb8111a2
Alexandr <kachan.alexandr@gmail.com>
2021-07-26 22:54:57 +0300

    У каждого пользователя свой список

--- src/main/java/by/lex/lextodolist/controllers/TodoListController.java
+++ src/main/java/by/lex/lextodolist/controllers/TodoListController.java
@@ -2,6 +2,10 @@ package by.lex.lextodolist.controllers;
 
 import by.lex.lextodolist.persist.TodoItem;
 import by.lex.lextodolist.persist.TodoItemRepository;
+import by.lex.lextodolist.persist.User;
+import by.lex.lextodolist.persist.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,36 +14,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
+
 @Controller
 public class TodoListController {
+    private static final Logger logger = LoggerFactory.getLogger(TodoListController.class);
 
-    private TodoItemRepository repository;
+    private final TodoItemRepository repository;
+    private final UserRepository userRepository;
+
 
     @Autowired
     //конструктор который дает доступ к репозиторию
-    public TodoListController(TodoItemRepository repository) {
+    public TodoListController(TodoItemRepository repository, UserRepository userRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public String indexPage(Model model){
-        model.addAttribute("items", repository.findAll());
+    public String indexPage(Model model, Principal principal) {
+
+        logger.info("Имя пользователя :{} ", principal.getName());
+
+        model.addAttribute("items", repository.findByUserUsername(principal.getName())); //привязка каждому пользователю своего списка по username
         model.addAttribute("item", new TodoItem());
         return "index";
     }
+
     @PostMapping
-    public String newTodoItem (TodoItem todoItem){
+    public String newTodoItem(TodoItem todoItem, Principal principal) {
+
+        logger.info("Имя пользователя :{} ", principal.getName());
+
+        User user = userRepository.findByUsername(principal.getName()).get();
+        todoItem.setUser(user);
+
         repository.save(todoItem);
         return "redirect:/";
     }
 
     @DeleteMapping("/{id}")
-    public String deleteTodoItem(@PathVariable("id") Long id){
+    public String deleteTodoItem(@PathVariable("id") Long id) {
         repository.deleteById(id);
         return "redirect:/";
     }
-
-
 
 
 }
--- src/main/java/by/lex/lextodolist/persist/TodoItemRepository.java
+++ src/main/java/by/lex/lextodolist/persist/TodoItemRepository.java
@@ -4,6 +4,12 @@ package by.lex.lextodolist.persist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
+    List<TodoItem> findByUserUsername(String username);
+
+
 }
